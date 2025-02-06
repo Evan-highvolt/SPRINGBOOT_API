21c3f4ac4b15503374a58c75517b19f7c0e11128
Evan-highvolt <evan.aljundi24@gmail.com>
2025-02-06 13:34:16 +0100

    Exclure Junit de pom

--- src/test/java/fr/afpa/pompey/cda/api/ApiApplicationTests.java
+++ src/test/java/fr/afpa/pompey/cda/api/ApiApplicationTests.java
@@ -2,8 +2,10 @@ package fr.afpa.pompey.cda.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles
 class ApiApplicationTests {
 
 	@Test
